ec27b8221501a8dc5ff93418811c3f28bbb565a1
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-06-09 23:08:21 -0300

    Add basic validation schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,9 @@
+from enum import Enum
 from pydantic import BaseModel
+
+class Status(Enum):
+    SUCCESS = "Success"
+    FAILED = "Failed"
 
 class FruitBase(BaseModel):
     name: str
@@ -6,6 +11,20 @@ class FruitBase(BaseModel):
 
 class FruitCreate(FruitBase):
     pass
+
+class FruitCreatedResponse(FruitBase):
+    id: int
+    Status: Status
+
+    class Config:
+        orm_mode = True
+
+class GetFruitResponse(FruitBase):
+    Status: Status
+    id: int
+    
+    class Config:
+        orm_mode = True
 
 class Fruit(FruitBase):
     id: int
